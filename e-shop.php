780f4beda1805480b7438029fcbebbf43de98f86
simonedimeglio <simonedimeglio@icloud.com>
2021-09-07 16:39:18 +0200

    all steps completed

--- e-shop.php
+++ e-shop.php
@@ -1,5 +1,6 @@
 <?php
-
+require_once('product.php');
+require_once('users.php');
 /* 
 TRACCIA ESERCIZIO
 1. creiamo l'eshop
@@ -18,6 +19,8 @@ class EShop {
     public $url; // String
     public $isOnline = true; // Boolean
     public $products = []; // Array
+    public $users = []; // Array
+
     
     // CONSTRUCT - assegno nome e url all'eShop alla sua costruzione
     public function __construct(string $eshopName, string $webUrl)
@@ -34,59 +37,32 @@ class EShop {
         return $this->products;
     }
 
-}
-// ------------------------------------ // 
-class Product {
+    public function addUsers(User $userName) {
+        $this->users[] = $userName;
+    }
 
-    public $name; // String
-    public $price; // Number
-    public $category; // String
-    public $isAvailable = true; // Boolean
-    
-}
+    public function getUsers() {
+        return $this->users;
+    }
 
-class TechProduct extends Product {
+    public function insertCreditCard() {
+        return $this->creditCardNumber;
+    }
 
-    public $haveCharger = false; // Boolean
-    public $haveHeadphones = false; // Boolean
-    public $isNew = true; // Boolean
-    public $isRefurbished = false; // Boolean
-    public $model; // String
+    public function buyAProduct(Product $productName) {
 
-}
+        // trovo la key via array_search
+        $keyProductToRemove = array_search($productName, $this->products);
+        // tolgo dall'array via unset
+        unset($this->products[$keyProductToRemove]);
 
-class Services extends Product {
-    
-    public $matter; 
+    }
 
 }
-// ------------------------------------ // 
-class User {
-
-    public $name; // String
-    public $surname; // String
-    public $eMail; // String
-    public $telephone; // Number
-    public $discount = 0; // Number
-    public $creditCardNumber; // Number
-    public $isCreditCardExpired = false; // Boolean
-
-}
-
-class PremiumUser extends User {
-
-    public $discount = 50; // Number
-    public $isPremium = true; // Boolean
-
-}
-
-// ------------------------------------ // 
-
-
 
 // STEP 1 - Creo l'eShop
 $eShop = new Eshop ('Apple Store', 'www.apple.it');
-echo('Esercizio PHP OOP - eShop<br><br>');
+echo('Esercizio PHP OOP - eShop<br><br> eShop:<br>');
 var_dump($eShop);
 echo('<br><hr><br>');
 
@@ -98,28 +74,28 @@ $firstProduct->category = 'Cover';
 $firstProduct->model = 'Leather Case';
 // var_dump($firstProduct);
 $firstTechProduct = new TechProduct;
-$firstTechProduct->name = 'iPhone';
+$firstTechProduct->name = 'iPhone 12 Pro Max';
 $firstTechProduct->price = 899;
 $firstTechProduct->category = 'Smartphone';
-$firstTechProduct->model = '12 Pro Max';
+$firstTechProduct->model = 'A14 Bionic';
 // var_dump($firstTechProduct);
 $secondTechProduct = new TechProduct;
-$secondTechProduct->name = 'iPhone';
+$secondTechProduct->name = 'iPhone 12 Mini';
 $secondTechProduct->price = 799;
 $secondTechProduct->category = 'Smartphone';
-$secondTechProduct->model = '12 mini';
+$secondTechProduct->model = 'A14 Bionic';
 // var_dump($secondTechProduct);
 $thirdTechProduct = new TechProduct;
-$thirdTechProduct->name = 'MacBook';
+$thirdTechProduct->name = 'MacBook Pro';
 $thirdTechProduct->price = 1499;
 $thirdTechProduct->category = 'Notebook';
-$thirdTechProduct->model = 'Pro M1';
+$thirdTechProduct->model = 'M1';
 // var_dump($thirdTechProduct);
 $fourthTechProduct = new TechProduct;
-$fourthTechProduct->name = 'MacBook';
+$fourthTechProduct->name = 'MacBook Air';
 $fourthTechProduct->price = 1199;
 $fourthTechProduct->category = 'Notebook';
-$fourthTechProduct->model = 'Air M1';
+$fourthTechProduct->model = 'M1';
 // var_dump($fourthTechProduct);
 $fifthTechProduct = new TechProduct;
 $fifthTechProduct->name = 'iMac';
@@ -128,10 +104,10 @@ $fifthTechProduct->category = 'Personal Computer';
 $fifthTechProduct->model = 'M1';
 // var_dump($fifthTechProduct);
 $sixthTechProduct = new TechProduct;
-$sixthTechProduct->name = 'iPad';
+$sixthTechProduct->name = 'iPad Pro';
 $sixthTechProduct->price = 669;
 $sixthTechProduct->category = 'Tablet';
-$sixthTechProduct->model = 'Air';
+$sixthTechProduct->model = 'M1';
 // var_dump($sixthTechProduct);
 $firstService = new Services;
 $firstService->name = 'Apple Music';
@@ -146,7 +122,7 @@ $secondService->category = 'Services';
 $secondService->matter = 'Cloud Storage';
 // var_dump($secondService);
 
-// Aggiungo i prodotti all'eShop
+// STEP 3 - Aggiungo i prodotti all'eShop
 $eShop->addProduct($firstProduct);
 $eShop->addProduct($firstTechProduct);
 $eShop->addProduct($secondTechProduct);
@@ -156,4 +132,49 @@ $eShop->addProduct($fifthTechProduct);
 $eShop->addProduct($sixthTechProduct);
 $eShop->addProduct($firstService);
 $eShop->addProduct($secondService);
+echo('Prodotti:<br>');
 var_dump($eShop->getProducts()); // stampa
+
+// STEP 4 - Creo l'utente standard
+$firstUser = new User;
+$firstUser->name = 'Tim';
+$firstUser->surname = 'Cook';
+$firstUser->eMail = 'timcook@apple.com';
+$firstUser->telephone = 00333454424;
+$firstUser->creditCardNumber;
+$eShop->addUsers($firstUser);
+
+
+// STEP 5 - Creo l'utente premium
+$firstPremiumUser = new PremiumUser;
+$firstPremiumUser->name = 'Steve';
+$firstPremiumUser->surname = 'Jobs';
+$firstPremiumUser->eMail = 'hellosteve@apple.com';
+$firstPremiumUser->telephone = 00321447622;
+$firstPremiumUser->creditCardNumber;
+$eShop->addUsers($firstPremiumUser);
+echo('<hr><br>Lista Users:<br>');
+// var_dump($eShop->getUsers()); // stampa
+
+// STEP 6 - Inserisco la carta di credito per ognuno via "insertCreditCard"
+$firstUser->insertCreditCard = 3333234376994638;
+$firstPremiumUser->insertCreditCard = 6554879918113826;
+var_dump($eShop->getUsers()); // stampa
+
+// STEP 7 - l'utente normale acquista un prodotto 
+$eShop->buyAProduct($thirdTechProduct);
+echo("<br><hr><br>Lista dei prodotti dopo l'acquisto dell'Utente (Standard) :<br>");
+var_dump($eShop->getProducts()); // stampa
+
+// STEP 8 - l'utente premium acquista un prodotto 
+// applico lo sconto del 50% all'acquisto del prodotto da parte dell'utente Premium
+echo("<br><hr><br>Prodotto con prezzo aggiornato per utente premium :<br>");
+$thirdTechProduct->price = ($thirdTechProduct->price)/2;
+var_dump($thirdTechProduct);
+// l'utente premium esegue l'acquisto
+$eShop->buyAProduct($firstTechProduct);
+echo("<br><hr><br>Lista dei prodotti dopo l'acquisto dell'Utente (premium) :<br>");
+var_dump($eShop->getProducts()); // stampa
+
+
+
