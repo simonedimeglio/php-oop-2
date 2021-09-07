b73559b54c6ed0e3c5f73d681d4911581fae6b75
simonedimeglio <simonedimeglio@icloud.com>
2021-09-07 16:39:06 +0200

    users page (separated for best order)

---
+++ users.php
@@ -0,0 +1,21 @@
+<?php
+
+class User {
+
+    public $name; // String
+    public $surname; // String
+    public $eMail; // String
+    public $telephone; // Number
+    public $discount = 0; // Number
+    public $creditCardNumber; // Number
+    public $isCreditCardExpired = false; // Boolean
+    public $isPremium = false; // Boolean
+
+}
+
+class PremiumUser extends User {
+
+    public $discount = 50; // Number
+    public $isPremium = true; // Boolean
+
+}
